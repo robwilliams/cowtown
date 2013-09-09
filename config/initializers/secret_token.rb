a3293ef8717747284a2a9294d2fd01efe764a152
Robert Williams <rob@r-williams.com>
2013-09-09 20:15:02 +0100

    Added base rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cowtown::Application.config.secret_key_base = '6e9342ba444e9d4ee403928ffe1422099236b7d794bd3728bf081b688678987ffd0f81e0c0886d589c7ca18210026dc5fbbfd3a9649d7afd97eed113aceb43ec'
